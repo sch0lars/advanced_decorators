9b2f156ad73f36301bd9b22639299b49b2891239
sch0lars <tph1994@gmail.com>
2020-11-04 16:51:39 -0700

    Added advanced_decorators.py

---
+++ advanced_decorators.py
@@ -0,0 +1,15 @@
+def decorator(*args, **kwargs):     # Takes the decorator arguments.
+    def inner(func):                # Takes the function as a parameter.
+        def wrapper():
+            print('Decorator args:')
+            for arg in args:
+                print(f'\t{arg}')
+            print('Decorator kwargs:')
+            for key, value in kwargs.items():
+                print(f'\t{key}: {value}')
+        return wrapper
+    return inner
+
+@decorator('arg1', 'arg2', kwarg1='value_1', kwarg2='value_2')
+def function():
+    print('Hello, World!')
